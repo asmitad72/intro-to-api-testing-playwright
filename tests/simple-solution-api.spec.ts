ceae79445d1e6322b76c2a0cd6548c8b1b0bf176
Asmita <asmitad72@gmail.com>
2026-01-12 19:48:41 +0530

    Update API specifications

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -12,6 +12,16 @@ test('get order with correct id should receive code 200', async ({ request }) =>
   expect(response.status()).toBe(200)
 })
 
+test('get order with incorrect id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/-5')
+  // Log the response status, body and headers
+  console.log('response body:', await response.json())
+  console.log('response headers:', response.headers())
+  // Check if the response status is 400
+  expect(response.status()).toBe(400)
+})
+
 test('post order with correct data should receive code 201', async ({ request }) => {
   // prepare request body
   const requestBody = {
@@ -30,4 +40,48 @@ test('post order with correct data should receive code 201', async ({ request })
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
+})
+
+test('post order with incorrect data should receive code 400', async ({ request }) => {
+  // prepare request body
+  const requestBody = {
+    status: '123',
+    courierId: 0,
+    //customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 0,
+  }
+  // Send a POST request to the server
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: requestBody,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  //console.log('response body:', await response.json())
+  expect(response.status()).not.toBe(StatusCodes.OK)
+})
+
+test('update order status with invalid ID, should received an error', async ({ request }) => {
+  // prepare request body
+  const requestBody = {
+    status: 'OPEN',
+  }
+  // Send a POST request to the server
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/-5/status', {
+    data: requestBody,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.NOT_FOUND)
+})
+
+test('get test order time without headers, should receive an error', async ({ request }) => {
+  // Send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/time/1', {})
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
